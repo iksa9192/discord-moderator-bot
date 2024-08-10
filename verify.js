182537753341699db276466991385e38df695bea
iksa9192 <iksa9192@gmail.com>
2024-08-11 00:46:23 +0300

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,5 +1,5 @@
 module.exports = {
     code: "discord.gg/wicks",
-    token: "MTI3MTk0NzA0NjAzNDYwODI4NA.GYhrx7.kTdQ9Y6z9e-amf6zArcvmpgW7rltpK6sXbI1jA",
+    token: "MTI3MTk0NzA0NjAzNDYwODI4NA.GqhpYW.yALlnkHyPptnUnj9KkDKKugnmXRXhu0ISaubSM",
     PORT:3000
 };
